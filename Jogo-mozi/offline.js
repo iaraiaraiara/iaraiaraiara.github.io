5253a98de04736ec5e468d2023e6e6c179600a3d
Iara Kelly <49798187+iaraiaraiara@users.noreply.github.com>
2021-12-28 16:02:29 -0300

    Add files via upload

---
+++ Jogo-mozi/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1640718084,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/solidtile.png",
+		"images/jumpthrutile.png",
+		"images/tilemap.png",
+		"images/req-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/aldeao-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/sprite2-sheet2.png",
+		"images/sprite2-sheet3.png",
+		"images/sprite2-sheet4.png",
+		"images/botao-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/perso-sheet0.png",
+		"images/tiledbackground.png",
+		"media/y2meta.com - 3 . djonga - leal (128 kbps) (1) (online-audio-converter.com).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
